e1c3db0049d9d3e9dddcc9b5ce73c584ec0e6044
flavkupe <flavkupe@umich.edu>
2017-09-01 15:34:43 -0700

    Initial commit

---
+++ Assets/Scripts/SliceDetector.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SliceInfo
+{
+    public Vector3 Start;
+    public Vector3 End;
+}
+
+public class SliceDetector : MonoBehaviour {
+
+    private CooldownTimer timer = new CooldownTimer(1.0f, true);
+
+    private Vector3? startPos = null;
+    private Vector3? endPos = null;
+    private bool swiping = false;
+
+    private TrailRenderer trail;
+
+    public float MaxSwipeDist = 5.0f;
+    public float SwipeSpeed = 1.0f;
+    public float SwipeTimer = 0.5f;
+    public float MinDistance = 1.0f;
+
+    public SliceInfo? GetSlice()
+    {
+        if (!this.swiping || !this.startPos.HasValue || !this.endPos.HasValue)
+        {
+            return null;
+        }
+
+        return new SliceInfo() { Start = this.startPos.Value, End = this.endPos.Value };
+    }
+
+	// Use this for initialization
+	void Start () {
+        trail = this.GetComponent<TrailRenderer>();
+        this.timer.SetBaseline(SwipeTimer);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (this.swiping)
+        {
+            return;
+        }
+
+	    if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).SetZ(0.0f);
+            timer.Reset();            
+            this.transform.position = startPos.Value;
+        }
+
+        timer.Tick(Time.deltaTime);
+
+        if (startPos != null && (Input.GetMouseButtonUp(0) || timer.IsExpired))
+        {
+            Vector3 dest = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = dest - startPos.Value;
+
+            if (direction.magnitude > MinDistance)
+            {
+                float dist = Mathf.Min(direction.magnitude, MaxSwipeDist);
+                direction = direction.normalized * dist;
+                this.endPos = this.startPos + direction;
+                this.endPos = this.endPos.Value.SetZ(0.0f);
+                this.swiping = true;
+                StartCoroutine(Swipe());
+            }
+            else
+            {
+                timer.SetInactive();
+                this.startPos = null;
+            }
+        }
+	}
+
+    IEnumerator Swipe()
+    {        
+        this.timer.SetInactive();
+        while ((this.endPos.Value - this.transform.position).magnitude > 0.2f)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, this.endPos.Value, SwipeSpeed);
+            yield return null;
+        }
+
+        this.startPos = null;
+        this.swiping = false;
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils {
+
+    public static void Shift(this Transform transform, float xShift, float yShift, float zShift)
+    {
+        transform.position = new Vector3(transform.position.x + xShift, transform.position.y + yShift, transform.position.z + zShift);
+    }
+
+    public static void SetX(this Transform transform, float newX)
+    {
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+
+    public static void SetY(this Transform transform, float newY)
+    {
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+    public static void SetZ(this Transform transform, float newZ)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
+    }
+
+    public static Vector3 SetX(this Vector3 vec, float newX)
+    {
+        return new Vector3(newX, vec.y, vec.z);
+    }
+
+    public static Vector3 SetY(this Vector3 vec, float newY)
+    {
+        return new Vector3(vec.x, newY, vec.z);
+    }
+
+    public static Vector3 SetZ(this Vector3 vec, float newZ)
+    {
+        return new Vector3(vec.x, vec.y, newZ);
+    }
+}
+
+
+/// <summary>
+/// A timer that can be used to track cooldowns and 
+/// durations of things.
+/// </summary>
+public class CooldownTimer
+{
+    /// <summary>
+    /// The goal of the timer. Once this time is reached,
+    /// the timer is "expired" (ie goes "ding!").
+    /// </summary>
+    private float baseLine = 0.0f;
+
+    private float currentTime = 0.0f;
+
+    public event EventHandler OnCooldownExpired;
+
+    /// <summary>
+    /// Ctor for creating an inactive timer. Set new baseline with
+    /// SetBaseline method.
+    /// </summary>
+    public CooldownTimer()
+    {
+        this.baseLine = 0.0f;
+    }
+
+    /// <summary>
+    /// Ctor for creating an active timer with timeout
+    /// occurring after time reaches 'baseLine'.
+    /// </summary>
+    /// <param name="baseLine">How long the timer must tick 
+    /// before cooldown expires</param>
+    public CooldownTimer(float baseLine, bool startsExpired = true)
+    {
+        this.baseLine = baseLine;
+        if (startsExpired)
+        {
+            this.currentTime = this.baseLine;
+        }
+    }
+
+    /// <summary>
+    /// Ticks the timer by some amount. Usually goes
+    /// in the Update loop using Time.deltaTime, but
+    /// delta can be anything. If delta is null, 
+    /// Time.deltaTime is used. Returns a reference to
+    /// this object, so you can do stuff like
+    /// if (timer.Tick().IsExpired) and other such things.
+    /// 
+    /// Only ticks if the cooldown is active (not expired and
+    /// non-zero baseLine). Immediately after expiry, the
+    /// OnCooldownExpired event is fired.
+    /// </summary>
+    /// <param name="delta"></param>
+    /// <returns></returns>
+    public CooldownTimer Tick(float? delta = null)
+    {
+        if (this.IsActive)
+        {
+            this.currentTime += delta ?? Time.deltaTime;
+            if (this.IsExpired)
+            {
+                // On the exact tick after expiry, this event fires.
+                if (this.OnCooldownExpired != null)
+                {
+                    this.OnCooldownExpired(this, new EventArgs());
+                }
+            }
+        }
+
+        return this;
+    }
+
+    /// <summary>
+    /// Whether or not the timer reached its goal
+    /// </summary>
+    public bool IsExpired { get { return this.currentTime >= baseLine; } }
+
+    /// <summary>
+    /// Whether or not this timer should tick.
+    /// </summary>
+    public bool IsActive { get { return this.baseLine > 0.0f && !this.IsExpired; } }
+
+    /// <summary>
+    /// Resets the timer to be used again.
+    /// </summary>
+    public void Reset()
+    {
+        this.currentTime = 0.0f;
+    }
+
+    /// <summary>
+    /// Changes the target time for the timer.
+    /// </summary>
+    /// <param name="baseLine"></param>
+    public void SetBaseline(float baseLine)
+    {
+        this.baseLine = baseLine;
+    }
+
+    public void SetInactive()
+    {
+        this.currentTime = this.baseLine;
+    }
+}
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+
+        protected set
+        {
+            instance = value;
+        }
+    }
+}
